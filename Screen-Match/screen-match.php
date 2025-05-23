3a14edf37022171ec9fc8f0a74fd80f186f933db
jupicinink <juliapicinini.2020@gmail.com>
2025-05-23 15:54:30 -0300

    atualizações do curso

--- Screen-Match/screen-match.php
+++ Screen-Match/screen-match.php
@@ -1,37 +1,76 @@
 <?php
+
 require __DIR__ . "/funcoes.php";
 
-echo "Bem vindo(a) ap screen match\n";
+echo "Bem-vindo(a) ao screen match!\n";
 
-
-// como cirar variavel
 $nomeFilme = "Top Gun - Maverick";
-$nomeFilme = "Thor Ragnarok";
-$nomeFilme = "Se beber não case";
-// variavel para entrada de dados pelo terminal  ?? se o argv[1] for nulo vai pegar por padrao o que estiver depois do ?? 
-$anoLancametno = $argv[1] ?? 2022;
 
+$anoLancamento = 2022;
+
+$quantidadeDeNotas = $argc - 1;
 $notas = [];
 
-for($i =1; $i < $argc; $i++){
-    $notas[] += (float) $argc[$i];
-};
+for ($contador = 1; $contador < $argc; $contador++) {
+    $notas[] = (float) $argv[$contador];
+}
 
+if ($quantidadeDeNotas > 0) {
+    $notaFilme = array_sum($notas) / $quantidadeDeNotas;
+} else {
+    $notaFilme = "Sem notas";
+}
+$planoPrime = true;
 
-// a expressão argc contem o numero de parametros passdados na linha de comando
-$notaFilme = array_sum($notas) / ($argc - 1);
-$planoPrime = True;
+$incluidoNoPlano = incluidoNoPlano($planoPrime, $anoLancamento);
 
+echo "Nome do filme: " . $nomeFilme . "\n";
+echo "Nota do filme: $notaFilme\n";
+echo "Ano de lançamento: $anoLancamento\n";
 
+exibeMensagemLancamento($anoLancamento);
 
-// essa expressao casa ou junta || 
 $genero = match ($nomeFilme) {
     "Top Gun - Maverick" => "ação",
-    "Thor Ragnarok" => "super-heroi",
-    "Se beber não case" => "comedia",
-    default => "genenro desconhecido",
+    "Thor: Ragnarok" => "super-herói",
+    "Se beber não case" => "comédia",
+    default => "gênero desconhecido",
 };
 
+echo "O gênero do filme é: $genero\n";
+
+$filme = [
+    "nome" => "Thor: Ragnarok",
+    "ano" => 2021,
+    "nota" => 7.8,
+    "genero" => "super-herói",
+];
 
 
+echo $filme["ano"];
 
+var_dump($notas);
+sort($notas);
+var_dump($notas);
+$menorNota = null;
+
+if (!empty($notas)) {
+    $menorNota = min($notas);
+    var_dump($menorNota);
+} else {
+    echo "Não há notas cadastradas para calcular a menor nota.\n";
+    var_dump($menorNota);
+}
+var_dump($menorNota);
+
+var_dump($filme['nome']);
+$posicaoDoisPontos = strpos($filme['nome'], ':');
+var_dump($posicaoDoisPontos);
+
+var_dump(substr($filme['nome'], 0, $posicaoDoisPontos));
+
+
+var_dump(json_decode('{"nome":"Thor: Ragnarok","ano":2021,"nota":7.8,"genero":"super-her\u00f3i"}') );
+
+$filmeComoStringJson = json_encode($filme);
+file_put_contents(__DIR__ . '/filme.json', $filmeComoStringJson);
--- Screen-Match/src/Modelo/Filme.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// criando um novo tipo
-class Filme {
-    public string $nome;
-    public int  $anoLancamento;
-    public float $nota;
-}
